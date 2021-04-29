fb49f326760c0ca3d45702a5405d14959efe6a01
dmarkie21 <69917132+dmarkie21@users.noreply.github.com>
2021-04-29 12:40:55 -0500

    Update run_lqg.py

--- run_lqg.py
+++ run_lqg.py
@@ -1,8 +1,10 @@
 import numpy as np
 import control
 from lqg import LQG
+import matplotlib.pyplot as plt
 
 import gym
+from gym.wrappers.monitoring import video_recorder
 from gym import wrappers
 from time import time
 
@@ -10,13 +12,16 @@ from time import time
 def main():
     # state matrix
     A = np.array([[0, 1, 0, 0],
-                  [0, 0, 0.716, 0],
+                  [0, 0, 7.8878, 0],
                   [0, 0, 0, 1],
-                  [0, 0, 15.76, 0]])
+                  [0, 0, 7.8878, 0]])
 
     # input matrix
-    B = np.array([[0], [0.9755], [0], [1.46]])
-    C = np.eye(4)
+    B = np.array([[0], [0.9091], [0], [-0.8049]])
+    C = np.array([[1, 0, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1]])
     D = np.zeros((4, 1))
     Q = np.array([[10, 0, 0, 0],
                   [0, 1, 0, 0],
@@ -25,19 +30,24 @@ def main():
     R = np.array([10])
 
     #covariance matrices for disturbance and noise
-    Vd = .001*np.eye(4);
-    Vn = .001;
+    Vd = .01*np.eye(4);
+    Vn = .01;
 
     #build lqg regulator
     lqg = LQG(A, B, C, D, Q, R, Vd, Vn)
 
-    env = gym.make('CartPole-v0')
-    env = wrappers.Monitor(env, './videos/' + str(time()) + '/')
+    env = gym.make('cp-custom-v0')
+    vid = video_recorder.VideoRecorder(env, './videos/' + str(time()) + '.mp4')
     env.env.seed(1)  # seed for reproducibility
-    obs = env.reset()
+    obs = env.reset(init_state=np.array([-1, 0.0, 1.1, 0.0]).reshape(-1, 1),
+                    end_state=np.array([0, 0.0, 0, 0.0]).reshape(-1, 1))
+
+
 
     for i in range(1000):
         env.render()
+        vid.capture_frame()
+
 
         noise = lqg.add_noise()
 
@@ -46,7 +56,6 @@ def main():
         Kf = lqg.kalman_gain(A, noisy_obs, C, Vd)
 
         pred_state = lqg.get_state_estimate(Kf, noisy_obs, obs, C, A)
-        print(pred_state)
 
         # get force direction (action) and force value (force)
         action, force = lqg.apply_state_controller(lqg.K, pred_state)
